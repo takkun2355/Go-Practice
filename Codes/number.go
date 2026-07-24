dc7a0abf5a85007fe3316c5f993aab971753f9fe
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-07-24 16:55:09 +0900

    Update number.go

--- Codes/number.go
+++ Codes/number.go
@@ -5,8 +5,11 @@ import (
 )
 
 func number() {
+	var calc int
 	var n [2]int
 	var op [1]string
+	calc = 0
+
 	fmt.Scan(&n[0], &op[0], &n[1])
 	fmt.Println("式:", n[0], op[0], n[1])
 
@@ -17,8 +20,24 @@ func number() {
 		fmt.Println(n[0] - n[1])
 	case "*":
 		fmt.Println(n[0] * n[1])
+	case "**":
+		if n[1] <= 1 {
+			fmt.Println(n[0])
+		} else {
+			calc += (n[0] * n[0])
+			for i := 0; i < n[1]-2; i++ {
+				calc += (calc * n[0])
+			}
+		}
+		fmt.Println(calc)
+	case "%":
+		fmt.Println(int(n[0]) % int(n[1]))
+	case "/%":
+		fmt.Println(int(n[0])/int(n[1]), n[0]%n[1])
+	case "//":
+		fmt.Println(int(n[0]) / int(n[1]))
 	case "/":
-		fmt.Println(n[0]/n[1], n[0]%n[1])
+		fmt.Println(float64(n[0]) / float64(n[1]))
 	default:
 		fmt.Println("不明なオプション")
 	}
